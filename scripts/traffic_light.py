6912d919be0104d6b1158f20dfaf144adf4a7cd8
yuu-Tueur <72367094+yuu-Tueur@users.noreply.github.com>
2021-01-13 12:08:01 +0900

    Update traffic_light.py

--- scripts/traffic_light.py
+++ scripts/traffic_light.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Int32
+
 rospy.init_node('trafficlight')
-pub = rospy.Publisher('count_up', Int32, queue_size = 1)
-rate = rospy.Rate(10)
 
 try:
 	while True:
